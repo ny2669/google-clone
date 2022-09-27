4a31b096c9d2217cfaa36c00dc449dc291e97c53
ny jones <ny_8@hotmail.co.uk>
2022-09-27 09:13:44 +0100

    main

--- pages/[id].js
+++ pages/[id].js
@@ -1,48 +1,48 @@
-import React from 'react'
+// import React from 'react'
 
-export const getStaticPaths = async () => {
+// export const getStaticPaths = async () => {
 
-const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=400738df53d5c4fa7&q=${id}`)
+// const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=400738df53d5c4fa7&q=${id}`)
   
-  const data = await res.json()
-  const paths = data.map(item => {
-  console.log(paths)
-    return{
-      params:{id : item.cacheId}
-    }
+//   const data = await res.json()
+//   const paths = data.map(item => {
+//   console.log(paths)
+//     return{
+//       params:{id : item.cacheId}
+//     }
   
-  })
+//   })
 
-  return{
-  paths: paths,
-  fallback: false,
+//   return{
+//   paths: paths,
+//   fallback: false,
   
-  }
+//   }
   
-  }
+//   }
 
 
-const Details = ({results}) => {
-  return (
-    <div></div>
-  )
-}
+// const Details = ({results}) => {
+//   return (
+//     <div></div>
+//   )
+// }
 
-export default Details
+// export default Details
 
 
-export const getStaticProps = async (context) => {
-    const id = context.params.id
- console.log(id)
+// export const getStaticProps = async (context) => {
+//     const id = context.params.id
+//  console.log(id)
 
-    const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=400738df53d5c4fa7&q=${id}`)
-    const data = await res.json()
+//     const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=400738df53d5c4fa7&q=${id}`)
+//     const data = await res.json()
     
     
-    return {
-    props:{
-      results: data
-    }
+//     return {
+//     props:{
+//       results: data
+//     }
 
-}
-}
+// }
+// }
